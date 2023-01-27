6a4177b68f5e357adf6b9887198943df23bf67a6
LeeShuyun <quaternion.design@gmail.com>
2023-01-27 17:00:25 +0800

    edit

--- src/main/java/dev/leeshuyun/SSFAssessment/controller/RedisRetrieveController.java
+++ src/main/java/dev/leeshuyun/SSFAssessment/controller/RedisRetrieveController.java
@@ -3,8 +3,20 @@ package dev.leeshuyun.SSFAssessment.controller;
 @RestController
 public class RedisRetrieveController {
     // returns response entities instead
-    @GetMapping(path="${orderId}")
-    public ResponseEntity<String> getOrder(@PathVariable String orderId){
-        
+    @GetMapping(path="{orderId}")
+    public ResponseEntity<String> getOrder(@PathVariable String orderId) throws IOException{
+        //use the services method to find the object by Id
+        PizzaOrder pOrder = pizzaServices.findById(orderId);
+
+        if (pOrder == null) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("");
+        }
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ms.toJSON().toString());
     }
 }
